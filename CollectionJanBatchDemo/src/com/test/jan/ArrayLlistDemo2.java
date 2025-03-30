5e79301edbbf680f9fcd8ecb41f9223b92e69695
Nilendra Yadav <nilendrayadav785@example.com>
2025-03-30 19:27:48 +0530

    arraylist code added for integer number

---
+++ CollectionJanBatchDemo/src/com/test/jan/ArrayLlistDemo2.java
@@ -0,0 +1,16 @@
+package com.test.jan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayLlistDemo2 {
+
+	public static void main(String[] args) {
+	
+		List<Integer>list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(20);
+        System.out.println(list);
+	}
+
+}
