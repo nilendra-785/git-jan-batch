641a5b253cdccb8ed27e9d5b12216446bc01f4cd
Nilendra Yadav <nilendrayadav785@example.com>
2025-03-30 15:42:44 +0530

    arraylist code added for city

---
+++ CollectionJanBatchDemo/src/com/test/jan/ArrayListDemo1.java
@@ -0,0 +1,19 @@
+package com.test.jan;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo1 {
+
+	public static void main(String[] args) {
+		
+		List<String>list = new ArrayList<>();
+     list.add("pune");
+     list.add("mumbai");
+     list.add("bangalore");
+     list.add("delhi");
+     System.out.println(list);
+	}
+
+}
