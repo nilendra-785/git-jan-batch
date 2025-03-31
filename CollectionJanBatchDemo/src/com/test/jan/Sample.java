1a6d1951faa34710c929d6a0e8e8759ab8ed29a0
Nilendra <nilendrayadav785@gmail.com>
2025-03-31 16:58:45 +0530

    Simple message added

---
+++ CollectionJanBatchDemo/src/com/test/jan/Sample.java
@@ -0,0 +1,11 @@
+package com.test.jan;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		//why Qs mark kyuki aapli file not commited
+		System.out.println("This is 18 Jan full stack batch");
+
+	}
+
+}
