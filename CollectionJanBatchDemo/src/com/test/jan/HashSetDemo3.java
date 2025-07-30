d1470d94b8d24463c2053a8b23fc62dd10d550e8
Nilendra <nilendrayadav785@gmail.com>
2025-07-30 20:57:33 +0530

    add new hashset code

---
+++ CollectionJanBatchDemo/src/com/test/jan/HashSetDemo3.java
@@ -0,0 +1,22 @@
+package com.test.jan;
+
+import java.util.HashSet;
+
+public class HashSetDemo3 {
+
+	public HashSet getHashSetDemo() {
+		HashSet hs = new HashSet();
+		hs.add(10);
+		hs.add(20);
+		hs.add(30);
+	return hs;
+	}
+	
+	public static void main(String[] args) {
+		
+		HashSetDemo3 hashSetDemo3 = new HashSetDemo3();
+		hashSetDemo3.getHashSetDemo();
+
+	}
+
+}
