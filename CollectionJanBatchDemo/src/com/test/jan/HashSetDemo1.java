2979b5951a47c80f49755102f7bffdbbd48378f6
Nilendra <nilendrayadav785@gmail.com>
2025-07-30 19:09:01 +0530

    HashSet string name added

---
+++ CollectionJanBatchDemo/src/com/test/jan/HashSetDemo1.java
@@ -0,0 +1,18 @@
+package com.test.jan;
+
+import java.util.HashSet;
+
+public class HashSetDemo1 {
+
+	public static void main(String[] args) {
+		
+		HashSet<String>hs = new HashSet<>();
+		hs.add("rohit");
+		hs.add("ajay");
+		hs.add("ramesh");
+		hs.add("karan");
+		System.out.println(hs);
+
+	}
+
+}
