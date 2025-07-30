bc11f0cce997eb5c18f08751f39869d3440f0cf9
Nilendra <nilendrayadav785@gmail.com>
2025-07-30 19:22:16 +0530

    HashSet Integer number added

---
+++ CollectionJanBatchDemo/src/com/test/jan/HashSetDemo2.java
@@ -0,0 +1,18 @@
+package com.test.jan;
+
+import java.util.HashSet;
+
+public class HashSetDemo2 {
+
+	public static void main(String[] args) {
+		
+		HashSet<Integer>hs =new HashSet<>();
+	  hs.add(1);
+	  hs.add(2);
+	  hs.add(3);
+	  hs.add(4);
+	  System.out.println(hs);
+
+	}
+
+}
