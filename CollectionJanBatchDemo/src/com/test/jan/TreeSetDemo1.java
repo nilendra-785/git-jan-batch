21409fc73df0dd06af4b49c4a313c684da2a42b5
Nilendra <nilendrayadav785@gmail.com>
2025-07-30 21:48:59 +0530

    succesfully sortet

---
+++ CollectionJanBatchDemo/src/com/test/jan/TreeSetDemo1.java
@@ -0,0 +1,19 @@
+package com.test.jan;
+
+
+import java.util.TreeSet;
+
+public class TreeSetDemo1 {
+
+	public static void main(String[] args) {
+	
+		TreeSet ts = new TreeSet<>();
+		ts.add(20);
+		ts.add(45);
+		ts.add(10);
+		System.out.println(ts);
+	
+
+	}
+
+}
